718088867d890bdcbca75d154bb3aaa2542cf6d5
Abraao Alcantara <sephiohff8@gmail.com>
2013-08-19 16:01:11 -0300

    Improved darkheart movement

--- src/entities/darkheart.js
+++ src/entities/darkheart.js
@@ -1,27 +1,42 @@
 DarkHeart = BaseEntity.extend({
 	defaults: {
-        
+
     },
     initialize: function(){
-		var X = Crafty.math.randomInt(210,800),
-			Y = 550,
+		var POSX = Crafty.math.randomInt(0,Crafty.viewport.width), // Initial x coordinate: any x possible value in screen
+			POSY = Crafty.viewport.height+300,  // Initial y coordinate: it´s created under the viewport
+			POSZ = 375,                        // Initial z coordinate
+			VPX = 400,						  // Perspective´s vanish point: x axis
+			VPY = 530,						 // Perspective´s vanish point: y axis
+			WIDTH = 250,					// Initial width
+			HEIGHT = 250,				   // Initial height
+			TAN = (VPY-POSY)/(VPX-POSX),  // Slope(tan) of a line that between vanish point and initial position
+			SPEED = 1,					 // Movement speed rate
 			model = this,
 			entity = Crafty.e("2D, "+gameContainer.conf.get('renderType')+", heart, darkHeart, Collision");
-		entity['poly'] = new Crafty.polygon([[0,0],[50,0],[50,50],[0,50]]);
+		entity['poly'] = new Crafty.polygon([[0,0],[WIDTH,0],[WIDTH,HEIGHT],[0,HEIGHT]]);
 		entity
-			.attr({x: X, y: Y, z: 375, h:50, w:50 })
+			.attr({x: POSX, y: POSY, z: POSZ, w: WIDTH, h: HEIGHT})
 			.collision(entity.poly)
 			.bind('EnterFrame',function() {
-				var rate = 0.1;
-				if(Math.round(this._h - rate)==0) {
-					this.destroy();
+				// At each frame, test if darkheart seeable 
+				if(Math.round(this._w)>0) {
+
+					// Calculate new values for instance attributes:
+					var newY = this._y - SPEED,         			// reduced using fixed rate(SPEED)		   
+						newX = ((newY-POSY)/TAN) + POSX,    		// change using new y-coordinate and TAN
+						newZ = this._z - SPEED, 					// reduced using fixed rate(SPEED)
+						sizeRate = ((newY-VPY)/(POSY-VPY)) * WIDTH; // used for new witdth and height; reduced using new y-coordinate
+
+					// Apply new values
+					this.attr({x: newX, y: newY, z: newZ, w: sizeRate, h: sizeRate });
+
+					// Calculate new coordinates used for collision tests (NEED TO BE FIXED!)
+					entity.poly.shift(sizeRate * -1,sizeRate * -1);
+
 				} else {
-				  this.attr({y: this._y - 0.02, x: this._x+(rate/2), w: this._w - rate, h: this._h - rate});
-				  if(this._z > 0) {
-					 entity.z = this._z - 1;
-				  }
-				  entity.poly.shift(rate * -1,rate * -1);
-				  //this.css('backgroundSize','cover');
+					// If DarkHeart width is the minimum possible(its not seeable), destroy itself 	
+					this.destroy();
 				}
 			  })
 			.setName('Dark heart');
