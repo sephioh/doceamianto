818c5d9a27d6fbf82d1f3dcee2c7af9fb0843dc0
heniotierra <heniotster@gmail.com>
2013-08-22 12:48:07 -0300

    fixed background color shifting and added degrade 'effect'

--- src/scenes/level01.js
+++ src/scenes/level01.js
@@ -16,7 +16,8 @@ Crafty.scene("level01", function() {
 		sc['hearts'] = [];
 		sc['delimiters'] = [];
 		sc['delays'] = Crafty.e("Delay");
-		sc['bckgrndFade'] = Crafty.e("2D, Canvas, TweenColor").attr({ x: 0, y: 0, w: 800, h: 600, z: 0 }).rgb({ r:255, g: 255, b: 255 });
+		sc['bckgrndFade'] = Crafty.e("2D, Canvas, TweenColor").attr({ x: 0, y: 0, w: 800, h: 600, z: 0 });
+		sc['bckgrndDegrade'] = Crafty.e("2D, Canvas, Sprite, degrade").attr({ x: 0, y: 0, w: 800, h: 600, z: 0 });
 		
 		//<hearts' loop> 
 		var summoning = sc.delays.delay(function() {
@@ -28,34 +29,38 @@ Crafty.scene("level01", function() {
 		
 		//<change background color>
 		var i = 0,
-			blue = { r: 0 , g: 0 , b: 255 },
-			yellow = { r: 255, g: 255, b: 0 },
-			white = { r: 255, g: 255, b: 255 };
+			white = { r: 255, g: 255, b: 255 },
+			yellow = { r: 255, g: 226, b: 78 },
+			pink = { r: 255, g: 77, b: 153 },
+			violet = { r: 128, g: 16, b: 216 },
+			green = { r: 117, g: 232, b: 7 };
 		  
+		sc.bckgrndFade.rgb(white);
+		
 		var backgroundChange = sc.delays.delay(function() {
 			switch(i) {
 				case 0: 
-					sc.bckgrndFade.tweenColor(blue, 255);
-					break;
-				case 1: 
 					sc.bckgrndFade.tweenColor(yellow, 255);
 					break;
+				case 1: 
+					sc.bckgrndFade.tweenColor(pink, 255);
+					break;
 				case 2: 
-					sc.bckgrndFade.tweenColor(white, 255);
+					sc.bckgrndFade.tweenColor(violet, 255);
+					break;
+				case 3: 
+					sc.bckgrndFade.tweenColor(green, 255);
 					break;
 			}
-			if(++i > 2) 
+			if(++i > 3) 
 				i = 0; 
 		},10000,-1);
 		//</change background color>
 		
 		//<delimiters>
 		var delimitersMap = {
-			bottom: { x: 210, y: 643, w: 590, h: 1, 	shape: [[0,0],[590,1]], isfloor: true }, 
 			left: 	{ x: 210, y: 242, w: 1, h: 400, 	shape: [[0,0],[1,400]], isfloor: false }, 
-			top: 	{ x: 210, y: 242, w: 590, h: 1, 	shape: [[0,0],[590,1]], isfloor: false }, 
-			right: 	{ x: 800, y: 242, w: 1, h: 400, 	shape: [[0,0],[1,400]], isfloor: false }, 
-			under: 	{ x: 210, y: 1700, w: 590, h: 1,	shape: [[0,0],[590,1]], isfloor: true } 
+			right: 	{ x: 800, y: 242, w: 1, h: 400, 	shape: [[0,0],[1,400]], isfloor: false }
 		};
 		
 		_.each(delimitersMap, function(obj) {
@@ -68,15 +73,13 @@ Crafty.scene("level01", function() {
 		});
 		//</delimiters>
 		
-		Crafty.viewport.centerOn(sc.player.get('entity'),1);
-		
 		Crafty.settings.modify("autoPause",true);
 		
 	});
 	
 	this.muchLove = Crafty.bind('TooMuchLove', function() {
 		var playerEnt = sc.player.get('entity');
-		sc.hrtsDelay.destroy();
+		sc.delays.destroy();
 		sc.delimiters[0].destroy();
 		Crafty.viewport.follow(playerEnt,0,0);
 		playerEnt._stopMoving();
