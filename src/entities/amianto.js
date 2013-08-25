0779d5cbb8b66eeff5f546e4febfecf5692f59e7
heniotierra <heniotster@gmail.com>
2013-08-25 17:27:05 -0300

    fixed an error

--- src/entities/amianto.js
+++ src/entities/amianto.js
@@ -62,11 +62,11 @@ Amianto = BaseEntity.extend({
 			.animate("AmiantoHittingTheGround", 0, 8, 7)
 			.bind('NewDirection', function (d) {
 				if (d.x > 0) {
-				  entity.stop().animate('AmiantoMovingRight', 64, -1);
+				  entity.stop().animate('AmiantoMovingRight', 32, -1);
 				} else if (d.x < 0) {
 				  entity.stop().animate('AmiantoMovingLeft', 32, -1);
 				} else {
-				  entity.stop().animate('AmiantoMovingTowards', 32, -1);
+				  entity.stop().animate('AmiantoMovingTowards', 64, -1);
 				}
 			  })
 			.bind('EnterFrame', function() {
