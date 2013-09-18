f7c2c28adef90c3cf538970b6a906f81d3bf611b
heniotierra <heniotster@gmail.com>
2013-09-18 12:22:56 -0300

    adapted 'loading' scene to be used for loading all other scenes, new support for loading text iles - json, xml, txt, etc -, can pass background color and audio to be played as arguments

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ window.onload = function() {
     var version = null,
 		today = new Date(),
 		lang = getUrlVars()['lang'];
-	lang = ((lang != 'en')?'pt':'en');
+		lang = ((lang != 'en')?'pt':'en');
 	
 	// Fix for cache
     if(gameContainer.env == 'dev') {
@@ -17,7 +17,7 @@ window.onload = function() {
     Crafty.canvas.init();
     
     require([
-	    "src/assets.js?v="+version+"",
+		"src/assets.js?v="+version+"",
 	    "src/config.js?v="+version+"",
 	    "src/lang/lang-"+lang+".js",
 		"src/components/ProgressBar.js",
@@ -28,27 +28,43 @@ window.onload = function() {
 	
 	assets = new Assets();
 	
-	gameContainer.setNextSceneInfo({ 
+	/*gameContainer.setNextSceneInfo({ 
 	  name: "level01",
 	  elements: [
 		  "src/components/TweenColor.js?v="+version+"",
 		  "src/entities/base/BaseEntity.js?v="+version+"",
 		]
+	});*/
+	
+	gameContainer.setNextSceneInfo({ 
+	  name: "level02",
+	  elements: [
+		  "text!src/scenes/tilemaps/level02.txt",
+		  "src/components/TweenColor.js?v="+version+"",
+		  "src/entities/base/BaseEntity.js?v="+version+"",
+		  "src/components/TiledLevelImporter.js",
+		  "src/components/camera.js",
+		],
 	});
 	
 	gameContainer['conf'] = new Config({});
 		
-	//the loading screen - that will be display while assets loaded
-	Crafty.scene("loading", function() {
+	// the loading screen - will be displayed while assets are loaded
+	Crafty.scene("loading", function(obj) { // { backgroundColor: '', soundToPlay: '' }
 	    // clear scene and interface
 	    sc = []; infc = [];   
+		
+		if (obj.backgroundColor!=undefined)
+			Crafty.backgroundColor(obj.backgroundColor);
+		if (obj.soundToPlay!=undefined)
+			Crafty.audio.play(obj.soundToPlay, -1);
 		
 		// set sprites for next scene
 		assets.createSprite(gameContainer.scene);
 		// set sounds for next scene
 		assets.createSound(gameContainer.scene);
 		
-	    var progressbar = Crafty.e("2D, ProgressBar, Persist")
+	    var progressbar = Crafty.e("2D, ProgressBar")
 			.attr({ x: 150, y : 140, w: 300, h: 25, z: 100 })
 			// this .progressBar(String eventName, Number blockCount, Number maxValue, 
 			// Boolean flipDirection, String emptyColor, String filledColor, String renderMethod)
@@ -56,17 +72,30 @@ window.onload = function() {
 		
 		// load takes an array of assets and a callback when complete
 	    Crafty.load(assets.getPaths(gameContainer.scene), 
-			function() {	
-				//when everything is loaded, run scene specified in gameContainer
-				require(gameContainer.elementsToLoad, function() {	   
-					progressbar.destroy();
-					
-					if (gameContainer.scene != undefined) {
-						
-						Crafty.scene(gameContainer.scene);
-						
+			function() {
+				// use eval for executing require(), also loading possible texts/maps
+				
+				var require_str = '', require_args = '', require_args_count = 0;
+				// build require_args string, if there are text files to load
+				_.each(gameContainer.elementsToLoad, function(ele){ 
+					// search for texts, first things to load
+					if( ele.lastIndexOf("text!") !== -1 ) {
+						require_args_count++;
+						if(require_args != '')
+							require_args += ', ';
+						require_args += 'arg' + require_args_count.toString();
 					}
 				});
+				
+				require_str = 
+ 				// require elements and execute callback
+				'require(gameContainer.elementsToLoad, function(' + require_args + ') { ' +
+				// if text files were loaded, add them to gameContainer.loadedStrings array
+				'if(arguments.length) _.each(arguments, function(a) { gameContainer.loadedStrings.push(a); });' +
+				// destroy progressbar and run the specified scene
+				'progressbar.destroy(); if (gameContainer.scene != undefined) { Crafty.scene(gameContainer.scene); } })';
+				
+				eval( '(' + require_str + ')' );
 			},
 			function(e) {
 				//progress
