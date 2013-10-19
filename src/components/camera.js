0f16f06ba7a70e057a885b09f13b451d470f30bc
Henio Tierra <heniotster@gmail.com>
2013-10-18 23:49:18 -0300

    reverted camera compnent to its previous state

--- src/components/camera.js
+++ src/components/camera.js
@@ -8,9 +8,26 @@ Crafty.c("Camera", {
         });
     },
     set: function(obj) {
+        xx = (-obj.x + Crafty.viewport.width / 2)
+        yy = (-obj.y + Crafty.viewport.height / 2)
+        Crafty.viewport.x = xx;
+        Crafty.viewport.y = yy;
+    }
+});
+
+/*Crafty.c("Camera", {
+    init: function() {},
+    camera: function(obj) {
+        this.set(obj);
+        var that = this;
+        obj.bind("Moved", function(location) {
+            that.set(location);
+        });
+    },
+    set: function(obj) {
         var xx = ((-obj._x + Crafty.viewport.width / 2) + obj._w/2),
 	    yy = ((-obj._y + Crafty.viewport.height / 2) + obj._h/2);
         Crafty.viewport.x = xx;
         Crafty.viewport.y = yy;
     }
-});
+});*/
