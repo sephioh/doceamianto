62ec2cb2405ccc47a64cd796ce9ce5d8c1100e55
Abraao Alcantara <sephiohff8@gmail.com>
2013-11-17 17:00:04 -0300

    Amianto03 can move with keyboard controls

--- src/entities/amianto03.js
+++ src/entities/amianto03.js
@@ -4,11 +4,12 @@ Amianto03 = BaseEntity.extend({
 		'initial_y' : 0,
 		'initial_z' : 0,
 		'initial_w' : 78,
-		'initial_h' : 96
+		'initial_h' : 96,
+		'initial_speed' : 3
     },
     initialize: function() {
 		var model = this,
-			entity = Crafty.e("2D, "+gameContainer.conf.get('renderType')+", amianto03, SpriteAnimation");
+			entity = Crafty.e("2D, "+gameContainer.conf.get('renderType')+", amianto03, SpriteAnimation, Multiway");
 		entity
 			// Set initial atributes
 			.attr({x: model.get('initial_x'),
@@ -25,6 +26,8 @@ Amianto03 = BaseEntity.extend({
 			.animate("MovingRight", 0, 3, 5)
 			.animate("PushingRigth", 0, 4, 5)
 			.animate("PushingLeft", 0, 5, 5)
+			// Controls
+			.multiway(model.get('initial_speed'), {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
 		model.set({'entity' : entity});
 	},
 });
