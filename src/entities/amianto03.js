fe8989871a00fa38c7ff97bc8a2792f05ec4dae9
Abraao Alcantara <sephiohff8@gmail.com>
2013-11-17 21:19:24 -0300

    Amianto have basic movement and initial animation

--- src/entities/amianto03.js
+++ src/entities/amianto03.js
@@ -26,8 +26,46 @@ Amianto03 = BaseEntity.extend({
 			.animate("MovingRight", 0, 3, 5)
 			.animate("PushingRigth", 0, 4, 5)
 			.animate("PushingLeft", 0, 5, 5)
+			// Default animation when amianto is created
+			.playAnimation("Standing", 20, -1)
 			// Controls
 			.multiway(model.get('initial_speed'), {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
+			.bind('Moved', function(prevPos) {
+				// if amianto move to right
+				if(this._x > prevPos.x)
+					moved = "right";
+				else
+				// if amianto move to up
+				if(this._y < prevPos.y) 
+					moved = "up";
+				else
+				// if amianto move to left
+				if(this._x < prevPos.x)
+					moved = "left";
+				else
+				// if amianto move to down
+				if(this._y > prevPos.y) 
+					moved = "down";
+
+				switch(moved) {
+					case "up" :
+						this.playAnimation("MovingVertically", 20, -1);
+						break;
+					case "down" :
+						this.playAnimation("MovingVertically", 20, -1);
+						break;
+					case "left":
+						this.playAnimation("MovingLeft", 20, -1);
+						break;
+					case "right": 
+						this.playAnimation("MovingRight", 20, -1);
+						break;
+				}
+			})
+			.bind('KeyUp', function(e) {
+				// When user release some key, amianto begins standing animation
+				this.playAnimation("Standing", 20, -1);
+			});
 		model.set({'entity' : entity});
 	},
 });
