769e329a20903b890a5426319eb6bc809aa1ab2f
Henio <heniotster@gmail.com>
2014-05-15 06:21:04 -0300

    trying to fix background issue

---
+++ src/components/Background.js
@@ -0,0 +1,38 @@
+Crafty.c("Background", {
+  
+	bgMoveRate: 15,
+	backgroundSectionSize: 965 - 1,
+	mapSectionSize: 1067.2,
+	playerEnt: null,
+	playerInitPos: null,
+	
+	init: function(){
+		this.requires("2D, "+gameContainer.conf.get('renderType')+", Image");
+		this.divisor = 1,
+		this.placement = null,
+		this.visible = false;
+	},
+	
+	startParallaxing: function() {
+		if ( this.placement !== null && this.playerInitPos !== null && this.playerEnt !== null ) {
+			return this.bind("PlayerMoved", function (prevPos) {
+				var bgI = Math.floor(this.playerEnt._x / this.mapSectionSize),
+				    shown = this.placement < bgI + 2 && this.placement > bgI - 2;
+				if (!this._visible && shown)
+					this.visible = true;
+				else if (this._visible && !shown)
+					this.visible = false;
+				if (prevPos._x !== this.playerEnt._x) {
+					var XD = (this.playerEnt._x - this.playerInitPos.x) / this.bgMoveRate;
+					this.x = XD + (this.backgroundSectionSize * this.placement) / this.divisor;
+				} else {
+					var YD = (this.playerEnt._y - this.playerInitPos.y) / this.bgMoveRate;
+					this.y = YD / this.divisor;
+				}
+			});
+		} else {
+			return false;
+		}
+	}
+	
+});
