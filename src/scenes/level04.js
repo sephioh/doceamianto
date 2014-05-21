6112724ced3c953be0bad1e13db606f9bae8fcb1
Henio <heniotster@gmail.com>
2014-05-21 16:20:07 -0300

    Revert "definetely fixed amianto03 not showing on the opposite wall"
    This reverts commit ffb0b9c302e5aaa1bbc0dd72ebf5f37fca5b878d.

--- src/scenes/level04.js
+++ src/scenes/level04.js
@@ -3,7 +3,7 @@ Crafty.scene("level04", function() {
 	//Crafty.background("#000000");
 	Crafty.background("#FCC6AC");
 	
-	//Crafty.audio.play("theme04", -1);
+	Crafty.audio.play("theme04", -1);
 	
 	//gameContainer.conf.set({'renderType': 'DOM'});
 	
@@ -24,85 +24,90 @@ Crafty.scene("level04", function() {
 	sc['playerMock'] = {},
 	sc['boss'] = {};
 	
-	var mapObj = JSON.parse(gameContainer.getSceneTexts()[0]), 
+	var mapObj1 = JSON.parse(gameContainer.getSceneTexts()[0]), 
 	    playerEnt = sc.player.getEntity(),
 	    fig_dev_y = 30,
-	    backgroundSectionSize = 964,		// original background section images' width: 965px
-	    mapSectionSize = 1067.2,
-	    playerEnt,
-	    playerInitPos = sc.player.get("startingPoint"),
-	    callPolicemen = function() {
-		    _.each(sc.pmSpawners, function(S) {
-			    S.spawn();
-		    });
-	    },
-	    setBackgrounds = function(arr) {
-		    var bgs = sc.bgs;
-			bg1 = "bg1", 
-			bg2 = "bg2", 
-			bg3 = "bg3", 
-			s = "-", 
-			l = "level04", 
-			ext = ".png", 
-			bgIndex = 0, 
-			imagesPath = Crafty.paths().images,
-			i = 0, 
-			len = arr.length;
+	    functions = {
+	      
+		backgroundSectionSize: 964,		// original background section images' width: 965px
+		mapSectionSize: 1067.2,
+		player: playerEnt,
+		playerInitPos: sc.player.get("startingPoint"),
+	  
+		callPolicemen: function() {
+			_.each(sc.pmSpawners, function(S) {
+				S.spawn();
+			});
+		},
+				
+		setBackgrounds: function(arr) {
+			var bgs = sc.bgs;
+			    bg1 = "bg1", 
+			    bg2 = "bg2", 
+			    bg3 = "bg3", 
+			    s = "-", 
+			    l = "level04", 
+			    ext = ".png", 
+			    bgIndex = 0, 
+			    imagesPath = Crafty.paths().images;
 
-		    for (; i<len; i++) {
-			if (arr[i].search(bg1) !== -1) {
-			    bgIndex = parseInt(arr[i].replace(bg1+s+l+s,"").replace(ext,""));
-			    bgs.bg1[bgIndex] = Crafty.e("Background").attr({ x: bgIndex * backgroundSectionSize, z: playerEnt._z - 2 }).image(imagesPath+arr[i]);
-			    bgs.bg1[bgIndex].placement = bgIndex;
-			} else if (arr[i].search(bg2) !== -1) {
-			    bgIndex = parseInt(arr[i].replace(bg2+s+l+s,"").replace(ext,""));
-			    bgs.bg2[bgIndex] = Crafty.e("Background").attr({ x: bgIndex * backgroundSectionSize, z: playerEnt._z - 3 }).image(imagesPath+arr[i]);
-			    bgs.bg2[bgIndex].placement = bgIndex;
-			} else if (arr[i].search(bg3) !== -1) {
-			    bgs.bg3 = Crafty.e("Background").attr({ x: 0, z: playerEnt._z - 4 , w: mapSectionSize * 20, h: 2464,  visible: true }).image(imagesPath+arr[i], "repeat");
-			}
-		    }
-		    return this;
-	    },
-	    startParallax = function() {
-		    var bg1 = sc.bgs.bg1,
-			bg2 = sc.bgs.bg2,
-			i,
-			p = 20,
-			bgMoveRate = 15,
-			bgI, shown, XD, YD, rightMargin, leftMargin;
-		    Crafty.bind("PlayerMoved", function (prevPos) {
-			    bgI = Math.floor(playerEnt._x / mapSectionSize),
-			    rightMargin = bgI > 14 ? 2 : 1,
-			    leftMargin = bgI > 17 ? 2 : 1;
-			    for (i = 0; i < p; i++) {
-				    shown = bg1[i].placement >= bgI - leftMargin && bg1[i].placement <= bgI + rightMargin;
-				    if (shown && !bg1[i]._visible) {
-					    bg1[i].visible = true,
-					    bg2[i].visible = true;
-				    } else if (!shown && bg1[i]._visible) {
-					    bg1[i].visible = false,
-					    bg2[i].visible = false;
-				    }
-				    if (prevPos._x !== playerEnt._x) {
-					    XD = (playerEnt._x - playerInitPos.x) / bgMoveRate;
-					    bg1[i].x = XD + (backgroundSectionSize * bg1[i].placement);
-					    bg2[i].x = (XD / 0.5) + (backgroundSectionSize * bg2[i].placement);
-				    } else {
-					    YD = (playerEnt._y - playerInitPos.y) / bgMoveRate;
-					    bg1[i].y = YD;
-					    bg2[i].y = YD / 0.5;
-				    }
+			for (var i = 0, len = arr.length; i<len; i++) {
+			    if (arr[i].search(bg1) !== -1) {
+				bgIndex = parseInt(arr[i].replace(bg1+s+l+s,"").replace(ext,""));
+				bgs.bg1[bgIndex] = Crafty.e("Background").attr({ x: bgIndex * this.backgroundSectionSize, z: playerEnt._z - 2 }).image(imagesPath+arr[i]);
+				bgs.bg1[bgIndex].placement = bgIndex;
+			    } else if (arr[i].search(bg2) !== -1) {
+				bgIndex = parseInt(arr[i].replace(bg2+s+l+s,"").replace(ext,""));
+				bgs.bg2[bgIndex] = Crafty.e("Background").attr({ x: bgIndex * this.backgroundSectionSize, z: playerEnt._z - 3 }).image(imagesPath+arr[i]);
+				bgs.bg2[bgIndex].placement = bgIndex;
+			    } else if (arr[i].search(bg3) !== -1) {
+				bgs.bg3 = Crafty.e("Background").attr({ x: 0, z: playerEnt._z - 4 , w: this.mapSectionSize * 20, h: 2464,  visible: true }).image(imagesPath+arr[i], "repeat");
 			    }
-		    });
-		    return this;
-	    };
+			}
+			return this;
+		},
+		
+		startParallax: function() {
+			var bg1 = sc.bgs.bg1,
+			    bg2 = sc.bgs.bg2,
+			    that = this,
+			    i,
+			    p = 20,
+			    bgMoveRate = 15,
+			    bgI, shown, XD, YD, rightMargin, leftMargin;
+			Crafty.bind("PlayerMoved", function (prevPos) {
+				bgI = Math.floor(that.player._x / that.mapSectionSize),
+				rightMargin = bgI > 14 ? 2 : 1,
+				leftMargin = bgI > 17 ? 2 : 1;
+				for (i = 0; i < p; i++) {
+					shown = bg1[i].placement >= bgI - leftMargin && bg1[i].placement <= bgI + rightMargin;
+					if (shown && !bg1[i]._visible) {
+						bg1[i].visible = true,
+						bg2[i].visible = true;
+					} else if (!shown && bg1[i]._visible) {
+						bg1[i].visible = false,
+						bg2[i].visible = false;
+					}
+					if (prevPos._x !== that.player._x) {
+						XD = (that.player._x - that.playerInitPos.x) / bgMoveRate;
+						bg1[i].x = XD + (that.backgroundSectionSize * bg1[i].placement);
+						bg2[i].x = (XD / 0.5) + (that.backgroundSectionSize * bg2[i].placement);
+					} else {
+						YD = (that.player._y - that.playerInitPos.y) / bgMoveRate;
+						bg1[i].y = YD;
+						bg2[i].y = YD / 0.5;
+					}
+				}
+			});
+			return this;
+		}
+	  
+	};
 	
-	sc.mm.prepTileset(mapObj.tilesets[0])
+	sc.mm.prepTileset(mapObj1.tilesets[0])
 	    .addMap()
 	    .one("TiledLevelLoaded", function(o) {
-		setBackgrounds(resources.get("level04").images);
-		startParallax();
+		functions.setBackgrounds(resources.get("level04").images).startParallax();
 		
 		Crafty.viewport.clampToEntities = false,
 		Crafty.viewport.follow(playerEnt, 0, 0),
@@ -136,7 +141,7 @@ Crafty.scene("level04", function() {
 		}),
 		playerEnt.gravity();
 	    })
-	    .buildTiledLevel(mapObj, gameContainer.conf.get('renderType'), false);
+	    .buildTiledLevel(mapObj1, gameContainer.conf.get('renderType'), false);
 	
 	sc.delimiters = [
 		Crafty.e("Delimiter, wall").attr({ x: 388, y: 256, w: 1, h: 320 }), 
@@ -178,8 +183,8 @@ Crafty.scene("level04", function() {
 	this.one("FigurantDied", function() {
 		this.trigger("Alert", 2);    
 		// call policemen now and each 20 seconds
-		callPolicemen();
-		sc.delays.delay(callPolicemen, 20000, -1);
+		functions.callPolicemen();
+		sc.delays.delay(functions.callPolicemen, 20000, -1);
 	});
 	
 	this.one("BossFight", function(){
@@ -202,10 +207,9 @@ Crafty.scene("level04", function() {
 			    this.destroy();
 			    playerEnt.alpha = 1;
 			    sc.delimiters[2].addComponent("wall");
-		    })
-		    .attach(playerEnt);
-		//playerEnt//.tween({ x: 19426 }, 5000);
-		sc.delays.cancelDelay(callPolicemen);
+		    });
+		playerEnt.tween({ x: 19426 }, 5000);
+		sc.delays.cancelDelay(functions.callPolicemen);
 		Crafty("Figurant").each(function(){ this.destroy(); });
 	});
 
