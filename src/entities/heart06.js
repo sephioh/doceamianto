f47f497e4010f2d286540d3477d665aebf311305
heniotierra <heniotster@gmail.com>
2014-10-09 15:37:46 -0300

    implemented level06 'rails' and hearts

--- src/entities/heart06.js
+++ src/entities/heart06.js
@@ -8,20 +8,20 @@ Heart = BaseEntity.extend({
 		var model = this,
 		    entity = Crafty.e("2D, " + gameContainer.conf.get('renderType') + ", heart, " + 
 			model.get("heartColor") + "Heart, Tween, Collision, Delay");
-		    if(model.get("initAttr"))
-			entity.attr(model.get("initAttr"));
-		    entity._nextStep = function(){
-			  ++this._step;
-			  var n = this._step;
-			  if(n + 1 > this._steps.length){
-			      this.destroy();
-			      return;
-			  }
-			  var next = { x: this._steps[n].x - this._w - this._w/2, y: this._steps[n].y - this._h };
-			  this.tween(next, 400);
-			  this.delay(function(){ this.one("TweenEnd", this._nextStep) }, 175);
-		    };
-		    entity._step = 0;
+		if(model.get("initAttr"))
+		    entity.attr(model.get("initAttr"));
+		entity._nextStep = function(){
+		      ++this._step;
+		      var n = this._step;
+		      if(n + 1 > this._steps.length){
+			  this.destroy();
+			  return;
+		      }
+		      var next = { x: this._steps[n].x - this._w - this._w/2, y: this._steps[n].y - this._h };
+		      this.tween(next, 400);
+		      this.delay(function(){ this.one("TweenEnd", this._nextStep) }, 175);
+		};
+		entity._step = 0;
 		model.set({'entity' : entity });
 	},
 	followSteps: function(rail){
